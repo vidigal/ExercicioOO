cdbcad87a48ab4810aa800e21e8a5c00b8c30f11
victorvidigal <victorvidigal@gmail.com>
2022-09-29 22:37:01 -0300

    Método comparar

--- src/Data.java
+++ src/Data.java
@@ -1,63 +1,95 @@
 public class Data {
 
-    private Integer dia;
-    private Integer mes;
-    private Integer ano;
+    public Integer dia;
+    private int mes;
+    public Integer ano;
 
-    public Data(Integer dia, Integer mes, Integer ano) {
-        if (this.isValida()) {
-            this.dia = dia;
-            this.mes = mes;
-            this.ano = ano;
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+
+        if (!this.isValida()) {
+            this.dia = 1;
+            this.mes = 1;
+            this.ano = 1;
         }
     }
 
     private boolean isValida() {
+        return  (this.isAnoValido() && this.isMesValido() && this.isDiaValido());
+    }
+
+    private boolean isAnoValido() {
         if (ano < 0 || ano > 9999) {
-            this.dia = 1;
-            this.mes = 1;
-            this.ano = 1;
             return false;
         }
+        return true;
+    }
 
+    private boolean isMesValido() {
         if (mes < 1 || mes > 12) {
-            this.dia = 1;
-            this.mes = 1;
-            this.ano = 1;
             return false;
         }
+        return true;
+    }
 
-        // meses com 31 dias: 1, 3, 5, 7, 8, 10, 12
-        // meses com 30 dias: 4, 6, 9, 11
-        // Meses com 28 ou 29 dias: 2
+    private boolean isDiaValido() {
         if (mes == 1 || mes == 3 || mes == 5 || mes == 7 || mes == 8 || mes == 10 || mes == 12) {
             if (dia < 1 || dia > 31) {
-                this.dia = 1;
-                this.mes = 1;
-                this.ano = 1;
                 return false;
             }
         }
 
         if (mes == 4 || mes == 6 || mes == 9|| mes == 11) {
             if (dia < 1 || dia > 30) {
-                this.dia = 1;
-                this.mes = 1;
-                this.ano = 1;
                 return false;
             }
         }
 
         if (mes == 2) {
             if (dia < 1 || dia > 29) {
-                this.dia = 1;
-                this.mes = 1;
-                this.ano = 1;
                 return false;
             }
         }
-
         return true;
+    }
+
+    public int compara(Data x) {
+        if (this.dia == x.dia && this.mes == x.mes && this.ano == x.ano) {
+            return 0;
+        }
+
+        if (this.ano > x.ano) {
+            return 1;
+        } else {
+            if (this.ano < x.ano) {
+                return -1;
+            }
+        }
+
+        // Ex.: 01/05/2022  -> 08/05/2022
+        if (this.ano == x.ano && this.mes == x.mes) {
+            if (this.dia > x.dia) {
+                return 1;
+            } else {
+                if (this.dia < x.dia) {
+                    return -1;
+                }
+            }
+        }
+
+        // Ex.: 01/06/2022  -> 08/05/2022
+        if (this.ano == x.ano) {
+            if (this.mes > x.mes) {
+                return 1;
+            } else {
+                return -1;
+            }
+        }
+
+        return -2;
+
     }
 
 }
--- src/Joao.java
+++ src/Joao.java
@@ -3,8 +3,16 @@ public class Joao {
 
     public static void main(String[] args) {
 
-        Data d1 = new Data(854564, 1, 2022);
+        Data d1 = new Data(25, 12, 128);
 
+        Data d2 = new Data(25, 12, 128);
+
+
+        if (d1.ano.equals(d2.ano)) {
+            System.out.println("é igual");
+        } else {
+            System.out.println("NÃO É IGUAL");
+        }
 
     }
 
